191dcb4d8648c92b5d1840dab7a9f53c53429ea3
Eferinte <Eferinte@163.com>
2023-04-05 20:29:10 +0800

    temp

--- src/main/java/com/ocg/Server/SimpleServer.java
+++ src/main/java/com/ocg/Server/SimpleServer.java
@@ -14,17 +14,21 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class SimpleServer {
 
     public static Game mainGame;
     public static SingleDuel duel;
+    public static List<Socket> connectController = new ArrayList<>();
 
 
-    public static String MsgAnalyze(String msg,Socket socket) {
-        msg = msg.substring(0,msg.length()-4);
-        String res = "echo-"+msg;
+    public static String MsgAnalyze(String msg, Socket socket) throws IOException {
+        msg = msg.substring(0, msg.length() - 4);
+        String res = "echo-" + msg;
         // 通用指令集
         if (msg.startsWith("ygo")) {
             int cmd = msg.indexOf('-');
@@ -32,98 +36,112 @@ public class SimpleServer {
                 msg = msg.substring(cmd + 1);
                 switch (msg.trim()) {
                     case "h", "help" -> {
-                        res = "-help 帮助\n-check 查看房间情况\n-join 加入房间\n-duel 开始决斗";
+                        res = "通用指令集:\nygo-help 帮助\nygo-check 查看房间情况\nygo-join 加入房间\nygo-duel 开始决斗\nchat-***** 聊天";
                         break;
                     }
-                    case "check"->{
+                    case "check" -> {
                         res = "当前玩家情况:";
                         int count = 0;
                         String[] client = new String[2];
-                        if(SingleDuel.players[0]!=null){
+                        if (SingleDuel.players[0] != null) {
                             count++;
                             client[0] = SingleDuel.players[0].ip;
                         }
-                        if(SingleDuel.players[1]!=null){
+                        if (SingleDuel.players[1] != null) {
                             count++;
                             client[1] = SingleDuel.players[0].ip;
                         }
-                        res+=count+"/2";
-                        if(client[0]!=null){
-                            res+="\n:DuelistA ip ="+client[0];
+                        res += count + "/2";
+                        if (client[0] != null) {
+                            res += "\n:DuelistA ip =" + client[0];
                         }
-                        if(client[1]!=null){
-                            res+="\n:DuelistB ip ="+client[1];
+                        if (client[1] != null) {
+                            res += "\n:DuelistB ip =" + client[1];
                         }
                         break;
                     }
-                    case "join" ->{
-                        res= "尝试加入房间...\n";
-                        if(SingleDuel.players[0]!=null){
-                            if(SingleDuel.players[0].ip.equals(socket.getInetAddress().toString())){
+                    case "join" -> {
+                        res = "尝试加入房间...\n";
+                        if (SingleDuel.players[0] != null) {
+                            if (SingleDuel.players[0].ip.equals(socket.getInetAddress().toString())) {
                                 res = "您已加入当前房间";
                                 break;
                             }
                         }
-                        if(SingleDuel.players[1]!=null){
-                            if(SingleDuel.players[1].ip.equals(socket.getInetAddress().toString())){
+                        if (SingleDuel.players[1] != null) {
+                            if (SingleDuel.players[1].ip.equals(socket.getInetAddress().toString())) {
                                 res = "您已加入当前房间";
                                 break;
                             }
                         }
-                        if(SingleDuel.players[0]!=null  && SingleDuel.players[1]!=null){
+                        if (SingleDuel.players[0] != null && SingleDuel.players[1] != null) {
                             res = "当前房间人数已满";
                             break;
                         }
-                        DuelPlayer dp = new DuelPlayer("DuelistA",socket.getInetAddress().toString());
+                        DuelPlayer dp = new DuelPlayer("DuelistA", socket.getInetAddress().toString());
                         dp.LoadDefault();
-                        if(duel!=null){
-                            res+=duel.JoinGame(dp);
+                        if (duel != null) {
+                            res += duel.JoinGame(dp);
                         }
                         break;
                     }
-                    case "duel"->{
-                        if(SingleDuel.players[0]!=null && SingleDuel.players[1]!=null){
+                    case "duel" -> {
+                        if (SingleDuel.players[0] != null && SingleDuel.players[1] != null) {
                             SingleDuel.players[0].LoadDefault();
                             SingleDuel.players[1].LoadDefault();
                             duel.UpdateDeck();
                             duel.StartDuel();
                             //TODO 启动duel
-                        }else{
+                        } else {
                             res = "房间人数不齐";
                         }
                         break;
                     }
                     default -> {
-                        res="未知指令";
+                        res = "未知指令";
                     }
                 }
-            }else{
+            } else {
                 res = "未知指令";
             }
         }
         // Duel指令集
-        if(msg.startsWith("ocg")){
+        if (msg.startsWith("ocg")) {
             int cmd = msg.indexOf('-');
             if (cmd != -1) {
                 msg = msg.substring(cmd + 1);
                 switch (msg.trim()) {
-                    case "surrender"->{
+                    case "surrender" -> {
                         break;
                     }
                 }
             }
         }
+        if (msg.startsWith("chat")) {
+            int head = msg.indexOf('-');
+            msg = msg.substring(head + 1);
+            Broadcast(msg,socket.getInetAddress().toString());
+            res = null;
+        }
         return res;
+    }
+
+    public static void Broadcast(String msg, String sponsor) throws IOException {
+        for (Socket s : connectController) {
+            s.getOutputStream().write((sponsor + ":" + msg + "over").getBytes());
+        }
     }
 
     public static void RunSocketServer(Game game, SingleDuel duel_mode) {
         mainGame = game;
         duel = duel_mode;
+
         try {
             ServerSocket server = new ServerSocket(8888);
             System.out.println("启动服务器....");
             while (true) {
                 Socket socket = server.accept();
+                connectController.add(socket);
                 System.out.println("客户端:" + socket.getInetAddress().getLocalHost() + "已连接到服务器");
                 new Thread(
                         new Runnable() {
@@ -144,12 +162,18 @@ public class SimpleServer {
                                             }
                                         }
                                         String content = sb.toString();
-                                        System.out.println("接收到客户端消息" + content.substring(0, content.length() - 4));
-
+//                                        System.out.println("接收到客户端消息" + content.substring(0, content.length() - 4));
+                                        if (content.substring(0, content.length() - 4).equals("exit")) {
+                                            socket.close();
+                                            break;
+                                        }
                                         //往客户端发送数据
                                         String nowTime = (new Date()).toString();
-                                        socket.getOutputStream().write((MsgAnalyze(content,socket) + "over").getBytes("UTF-8"));
-                                        socket.getOutputStream().flush();
+                                        String res = MsgAnalyze(content, socket);
+                                        if (res != null) {
+                                            socket.getOutputStream().write((res + "over").getBytes("UTF-8"));
+                                            socket.getOutputStream().flush();
+                                        }
                                     }
                                 } catch (IOException e) {
                                     e.printStackTrace();
